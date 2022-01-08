0b5e620032afe0ba98ee9469d0cfbff84cf56f1f
Ingyinmoe <arkar84848484@gmail.com>
2022-01-08 21:54:29 +0630

    edu pj dn

---
+++ app.js
@@ -0,0 +1,11 @@
+let NavLink = document.getElementById('NavLink');
+
+function shownav(){
+    NavLink.style.right = '0';
+    NavLink.style.width = '200px';
+};
+
+function hidenav(){
+    NavLink.style.right = '-200px';
+    NavLink.style.width = '0';
+};
